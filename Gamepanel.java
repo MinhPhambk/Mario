7134a2d46c0022d1a599b175b378909d1660e86c
Nho Dũng <92044633+NhoDung@users.noreply.github.com>
2023-04-20 16:40:21 +0700

    clone code

--- Gamepanel.java
+++ Gamepanel.java
@@ -3,13 +3,15 @@ import javax.swing.JPanel;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Gamepanel extends JPanel implements Runnable, KeyListener{
     private static final long serialVersionUID = 1L;
 
-    public static final int WIDTH = 500, HEIGHT = 500;
+    public static final int WIDTH = 400, HEIGHT = 400;
 
     private Thread thread;
 
@@ -20,14 +22,25 @@ public class Gamepanel extends JPanel implements Runnable, KeyListener{
     private BodyPart b;
     private ArrayList<BodyPart> snake;
 
-    private int xCoor = 10, yCoor = 10, size = 5;
+    private Apple apple;
+    private ArrayList<Apple> apples;
+
+    private Random r;
+
+    private int xCoor = 15, yCoor = 15, size = 25;
     private int ticks = 0;
 
     public Gamepanel(){
+        setFocusable(true);
+
         setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        addKeyListener(this);
 
         snake = new ArrayList<BodyPart>();
+        apples = new ArrayList<Apple>();
 
+        r = new Random();
+        
         start();
     }
 
@@ -54,8 +67,8 @@ public class Gamepanel extends JPanel implements Runnable, KeyListener{
         if (ticks > 250000) {
             if(right) xCoor++;
             if(left) xCoor--;
-            if(up) yCoor++;
-            if(down) yCoor--;
+            if(up) yCoor--;
+            if(down) yCoor++;
 
             ticks = 0;
 
@@ -63,7 +76,35 @@ public class Gamepanel extends JPanel implements Runnable, KeyListener{
             snake.add(b);
 
             if (snake.size() > size) {
-                snake.remove(0));
+                snake.remove(0);
+            }
+            if (apples.size() ==0) {
+                int xCoor = r.nextInt(39);
+                int yCoor = r.nextInt(39);
+
+                apple = new Apple(xCoor, yCoor, 10);
+                apples.add(apple);
+            }
+
+            for (int i = 0; i < apples.size(); i++) {
+                if (xCoor == apples.get(i).getxCoor() && yCoor == apples.get(i).getyCoor()) {
+                    size++;
+                    apples.remove(i);
+                    i++;
+                }
+            }
+            for (int i = 0; i < snake.size(); i++) {
+                if (xCoor == snake.get(i).getxCoor() && yCoor == snake.get(i).getyCoor()) {
+                    if (i != snake.size()-1) {
+                        System.out.println("GAME OVER");
+                        stop();
+                    }
+                }
+            }
+
+            if (xCoor < 0 || xCoor > 39 || yCoor < 0 || yCoor > 39) {
+                System.out.println("GAME OVER");
+                stop();
             }
         }
 
@@ -83,6 +124,9 @@ public class Gamepanel extends JPanel implements Runnable, KeyListener{
         for (int i = 0; i < snake.size(); i++) {
             snake.get(i).draw(g);
         }
+        for (int i = 0; i < apples.size(); i++) {
+            apples.get(i).draw(g);
+        }
     }
 
     @Override
@@ -92,4 +136,38 @@ public class Gamepanel extends JPanel implements Runnable, KeyListener{
             repaint();
         }
     }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        int key = e.getKeyCode();
+        if (key == KeyEvent.VK_RIGHT && !left) {
+            right = true;
+            up = false;
+            down = false;
+        }
+        if (key == KeyEvent.VK_LEFT && !right) {
+            left = true;
+            up = false;
+            down = false;
+        }
+        if (key == KeyEvent.VK_UP && !down) {
+            up = true;
+            left = false;
+            right = false;
+        }
+        if (key == KeyEvent.VK_DOWN && !up) {
+            down = true;
+            left = false;
+            right = false;
+        }
+    }
+    @Override
+    public void keyReleased(KeyEvent e) {
+        
+    }
+    @Override
+    public void keyTyped(KeyEvent e) {
+        
+    }
+
 }
